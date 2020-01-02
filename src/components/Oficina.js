498a318cb02b70d7a710aa3616846e768904aaf6
Eric Birkner <ericbirkner@gmail.com>
2020-01-02 12:29:39 -0300

    Update Oficina.js

--- src/components/Oficina.js
+++ src/components/Oficina.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react';
 
+function Oficina(props) {
+  //props.data.results_name
 
-class Oficina extends Component {
-  constructor(props) {
-    super(props);
-    this.sumValues= this.sumValues.bind(this);
-  }
-
-  sumValues(arr){
-    return Object.values(arr).reduce((a, b) =>
-      a + b.waiting
-    );
+  function sumValues(obj){
+    console.log(obj);
+    return Object.values(obj).reduce((a, b) =>
+     a + b.waiting
+   );
   }
 
 
 
-  render() {
-    return (
+  return(
+
       <div className="col-lg-3 col-md-4 col-sm-12 p-1 mb-1">
-        <div className={this.props.data.online ? "box" : "box disabled"}>
-          <div className="name">{this.props.data.name}</div>
+        <div className={props.data.online ? "box" : "box disabled"}>
+          <div className="name">{props.data.name}</div>
           <div className="footer">
-            <i className="fa fa-user" aria-hidden="true"></i> {() => this.sumValues(this.props.data.lines)}
+            <i className="fa fa-user" aria-hidden="true"></i> {() => sumValues(props.data.lines)}
               &nbsp;&nbsp;
             <i className="fa fa-clock-o" aria-hidden="true"></i> 2:22
           </div>
         </div>
       </div>
-    );
-  }
+    )
 }
 
 export default Oficina;
