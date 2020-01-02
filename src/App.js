0ec6c37b7a7932911a47332bcb31ead3d9306103
Eric Birkner <ericbirkner@gmail.com>
2020-01-02 12:06:18 -0300

    ajsute

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+import axios from 'axios';
+import Oficina from './components/Oficina';
 
 class App extends Component {
+
+  state = {
+    oficinas: []
+  }
+
+  componentDidMount() {
+    axios.get('https://boiling-mountain-49639.herokuapp.com/desafio-frontend')
+      .then(res => {
+        const oficinas = res.data;
+        this.setState({ oficinas });
+      })
+  }
+
   render() {
     return (
       <div className="App container-fluid">
@@ -10,27 +25,27 @@ class App extends Component {
           <div className="col-12 mt-3 mb-3"><img src={logo} className="App-logo" alt="logo" /></div>
         </div>
         <div className="row franja">
-          <div className="container pt-3">
-            <div className="col-lg-6 col-sm-12">
+          <div className="container pt-3 pl-0">
+            <div className="col-lg-6 col-sm-12 no-gutters">
               <div className="form-group has-search">
                 <span className="fa fa-search form-control-feedback"></span>
-                <input type="text" class="form-control" placeholder="Buscar Sucursal"/>
+                <input type="text" className="form-control" placeholder="Buscar Sucursal"/>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="container mt-4">
-          <div className="box col-lg-3 col-md-4 col-sm-12 p-0 m0">
-            <div className="name">Servipag Ejercito Libertador</div>
-            <div className="footer">
-              <i class="fa fa-user" aria-hidden="true"></i> 40
-                &nbsp;&nbsp;
-              <i class="fa fa-clock-o" aria-hidden="true"></i> 2:22
-            </div>
-          </div>
 
+        <div className="container mt-4">
+
+          <div className="row">
+            { this.state.oficinas.map(oficina =>
+               <Oficina data={oficina}></Oficina>
+            )}
+          </div>
         </div>
+
+
 
 
       </div>
